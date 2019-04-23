364b878ccececeacf0aaeb8435edd28e9009d40d
IT Smiths <ishu.bys@gmail.com>
2019-04-23 17:07:52 +0530

    Remove Extra character from url

--- common_function.php
+++ common_function.php
@@ -43,7 +43,6 @@ function getinboundLinks($domain_name) {
 			}
 		}
 		if(!empty($inbound_links)){
-		
 			foreach ($inbound_links as $key => $value) {
 			 	$url = $value;
 				$url_without_www = str_replace('https://','',$url);
@@ -51,33 +50,33 @@ function getinboundLinks($domain_name) {
 				$url_without_www = str_replace('www.','',$url_without_www);
 			 	$url_without_www = str_replace(strstr($url_without_www,'/'),'',$url_without_www);
 				$url_without_www = trim($url_without_www);
-				$input = @file_get_contents($url);
-
+				$inputs = @file_get_contents($url);
+				$links = [];
 			 	$regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>(.*)<\/a>";
 				$inbound = 0;
 				$outbound = 0;
 				$nonfollow = 0;
-				$links = [];
-				if(preg_match_all("/$regexp/siU", $input, $matches, PREG_SET_ORDER)) {
-					echo "<pre>";
-					print_r($matches);
-					echo "</pre>";
-					foreach($matches as $match) {
-						if(!empty($match[2]) && !empty($match[3])) {
-							if(strstr(strtolower($match[2]),'URL:') || strstr(strtolower($match[2]),'url:') ) {
-								$nonfollow +=1;
-							} else if (strstr(strtolower($match[2]),$url_without_www) || !strstr(strtolower($match[2]),'http://')) {
-						     	$inbound += 1;
-						     	if($value != $match[2]){
-						     		$links[] = $match[2];
-						     	}
-						 	}
-							else if (!strstr(strtolower($match[2]),$url_without_www) && strstr(strtolower($match[2]),'http://')) {
-						     	$outbound += 1;
-						    }
+				if($inputs != ''){
+				
+					if(preg_match_all("/$regexp/siU", $inputs, $matchesdata, PREG_SET_ORDER)) {
+						foreach($matchesdata as $match) {
+							if(!empty($match[2]) && !empty($match[3])) {
+								if(strstr(strtolower($match[2]),'URL:') || strstr(strtolower($match[2]),'url:') ) {
+									$nonfollow +=1;
+								} else if (strstr(strtolower($match[2]),$url_without_www) || !strstr(strtolower($match[2]),'http://')) {
+							     	$inbound += 1;
+							     	if($value != $match[2]){
+							     		$links[] = $match[2];
+							     	}
+							 	}
+								else if (!strstr(strtolower($match[2]),$url_without_www) && strstr(strtolower($match[2]),'http://')) {
+							     	$outbound += 1;
+							    }
+							}
 						}
 					}
 				}
+
 			}
 		}
 		return $links;
